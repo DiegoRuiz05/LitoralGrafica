c0bf7470a31abcdc9c3ddbdf5d7ad920942145f4
DiegoRuiz05 <contatodiegosouzaruiz@gmail.com>
2024-08-29 16:43:30 -0300

    Sacolas plasticas finalizadas

--- src/main/java/com/example/sacolasprojeto/controller/MeuController.java
+++ src/main/java/com/example/sacolasprojeto/controller/MeuController.java
@@ -15,10 +15,33 @@ public class MeuController {
     return "SacolasPlasticas";
   }
 
+@GetMapping("/SacolasPlasticas")
+ public ModelAndView sacolasPlasticas() {
+    ModelAndView mv = new ModelAndView("SacolasPlasticas");
+    return mv;
+}
 
   @GetMapping("/bocaVazadaFoscaOrcamento")
   public ModelAndView bocaVazadaFoscaOrcamento() {
     ModelAndView mv = new ModelAndView("bocaVazadaFoscaOrcamento");
+    return mv;
+  }
+
+  @GetMapping("/BocaVazadaBrilhoOrcamento")
+    public ModelAndView bocaVazadaBrilhoOrcamento() {
+      ModelAndView mv = new ModelAndView("bocaVazadaBrilhoOrcamento");
+      return mv;
+  }
+
+  @GetMapping("/AlcaCamisetaOrcamento")
+  public ModelAndView alcaCamisetaOrcamento() {
+    ModelAndView mv = new ModelAndView("AlcaCamisetaOrcamento");
+    return mv;
+  }
+
+  @GetMapping("/AlcaFitaOrcamento")
+  public ModelAndView alcaFitaOrcamento() {
+    ModelAndView mv = new ModelAndView("AlcaFitaOrcamento");
     return mv;
   }
 
